4e75d6e641d85f6d4bb9853bb6c0dbc82cc8289f
alex08dunn <alex08dunn@gmail.com>
2018-10-04 18:41:19 -0400

    More testing

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ from database import db_session
 app = Flask(__name__)
 app.secret_key = os.environ['APP_SECRET_KEY']
 
+@app.route("/")
+def locateMe():
+    return render_template('index.html')
+
 @app.route("/signup", methods=('GET', 'POST'))
 def signup():
     form = SignupForm()
@@ -17,16 +21,12 @@ def signup():
         signup = Signups(name=form.name.data, email=form.email.data, date_signed_up=datetime.datetime.now())
         db_session.add(signup)
         db_session.commit()
-        return redirect(url_for('success'))
+        return redirect(url_for('/signup/success'))
     return render_template('signup.html', form=form)
 
-@app.route("/success")
+@app.route("/signup/success")
 def success():
     return "Thank you for signing up!"
-
-@app.route("/")
-def locateMe():
-    return render_template('index.html')
 
 
 if __name__ == '__main__':
