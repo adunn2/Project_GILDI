b4b94287a3f7809d2abd4f07f1885b7a9a53b92a
alex08dunn <alex08dunn@gmail.com>
2018-10-04 18:51:38 -0400

    Adding some key stuff for testing

--- app.py
+++ app.py
@@ -9,22 +9,23 @@ from database import db_session
 
 app = Flask(__name__)
 app.secret_key = os.environ['APP_SECRET_KEY']
+google_api_key = os.environ['GOOGLE_API_KEY']
 
-@app.route("/")
+@app.route("/geo")
 def locateMe():
-    return render_template('index.html')
+    return render_template('index.html', google_api_key=google_api_key )
 
-@app.route("/signup", methods=('GET', 'POST'))
+@app.route("/", methods=('GET', 'POST'))
 def signup():
     form = SignupForm()
     if form.validate_on_submit():
         signup = Signups(name=form.name.data, email=form.email.data, date_signed_up=datetime.datetime.now())
         db_session.add(signup)
         db_session.commit()
-        return redirect(url_for('/signup/success'))
+        return redirect(url_for('success'))
     return render_template('signup.html', form=form)
 
-@app.route("/signup/success")
+@app.route("/success")
 def success():
     return "Thank you for signing up!"
 
