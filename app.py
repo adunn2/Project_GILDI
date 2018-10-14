baaae15daf67e0e2035fecbf308c8a278928bb5a
evilbunny76 <adunn2@umbc.edu>
2018-10-14 14:20:38 -0400

    Testing more noaa data form chages

--- app.py
+++ app.py
@@ -49,12 +49,10 @@ def noaaWeatherData():
     form = noaaDataForm()
     payload = dict(datasetid='GHCND', locationid='ZIP:21113', startdate='2010-05-01', enddate='2010-05-02', limit=1000)
     if form.validate_on_submit():
-        location = "ZIP:" + form.zipCode.data
-        print(location + "  " + form.startDate.data + "  " + form.endDate.data)
-        weather_data = data.fetch_data(**payload)
-        # weather_data = data.fetch_data(datasetid='GHCND', locationid=location, startdate='2015-05-01', enddate='2018-05-02', limit=1000)
-    else:
-        weather_data = data.fetch_data(datasetid='GHCND', locationid='ZIP:21113', startdate='2010-05-01', enddate='2010-05-02', limit=1000)
+        payload['locationid'] = "ZIP:" + form.zipCode.data
+        print(payload)
+    #     weather_data = data.fetch_data(datasetid='GHCND', locationid='ZIP:21113', startdate='2010-05-01', enddate='2010-05-02', limit=1000)
+    weather_data = data.fetch_data(**payload)
 
     return render_template('noaa.html', form=form, noaaData=weather_data)
 
